794d6cb9f56a8edfb71a8c6ff77318f392a67dd9
lin yijie <yijie_lin@fzzixun.com>
2024-02-20 15:13:18 +0800

    chore: 配置打包格式

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ export default {
   input: 'src/index.js',  // 入口文件
   output: [
     {
-      file: 'dist/index.cjs.js',  // 输出文件
+      file: 'dist/index.umd.cjs',  // 输出文件
       format: 'cjs'  // 输出模块格式
     },
     {
-      file: 'dist/index.esm.js',  // 输出文件
+      file: 'dist/index.esm.mjs',  // 输出文件
       format: 'es'  // 输出模块格式
     }
   ]
