e8b89d01d40879780cb0e48f53b6c14d5426a778
lin yijie <yijie_lin@fzzixun.com>
2024-02-20 15:20:10 +0800

    update: .releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 export default {
-  branches: ["main"], // 指定在哪个分支下要执行发布操作
+  branches: ["master"], // 指定在哪个分支下要执行发布操作
   plugins: [
     "@semantic-release/commit-analyzer", // 解析 commit 信息，默认就是 Angular 规范
     "@semantic-release/release-notes-generator",
